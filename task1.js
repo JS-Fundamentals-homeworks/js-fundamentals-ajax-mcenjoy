c1d09746eb66cde2bce1a7c7272188cc034ded83
Michael <mcenjoy@gmail.com>
2025-02-27 02:35:01 +0200

    All tasks have been completed.

--- task1.js
+++ task1.js
@@ -1,7 +1,27 @@
-// Завдання: отримання даних про користувачів 
+// Завдання: отримання даних про користувачів
 // За допомогою засобі Fetch отримати інформацію про користувачів
-// за посиланням - https://jsonplaceholder.typicode.com/users 
+// за посиланням - https://jsonplaceholder.typicode.com/users
 // Імена користувачів відобразити в ненумерованому списку ul.usersList,
 // який створений у файлі index.html
 // Запустити програму за допомогою Live Server
-// Перевірити за допомогою команди npm tests/task1.test.js 
+// Перевірити за допомогою команди node tests/task1.test.js
+
+document.addEventListener('DOMContentLoaded', async () => {
+  const userList = document.querySelector('ul.usersList');
+  if (!userList) {
+    console.error("Element with class 'usersList' not found!");
+    return;
+  }
+
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    if (!response.ok) {
+      throw new Error(`Error ${response.status}`);
+    }
+
+    const users = await response.json();
+    userList.innerHTML = users.map((user) => `<li>${user.name}</li>`).join('');
+  } catch (error) {
+    console.error('Error fetching users:', error);
+  }
+});
--- task2.js
+++ task2.js
@@ -5,3 +5,33 @@
 // відобразити у тезі #userCity
 // Запустити програму потрібно за допомогою Live Server
 // Перевірити правильність програми - команда node tests/task2.test.js
+
+document.addEventListener('DOMContentLoaded', () => {
+  const userNameInput = document.getElementById('userNameInput');
+  const getUserButton = document.getElementById('getUserButton');
+  const userCity = document.getElementById('userCity');
+
+  getUserButton.addEventListener('click', async () => {
+    const userName = userNameInput.value.trim().toLowerCase();
+    if (!userName) {
+      userCity.textContent = 'Please enter a username';
+      return;
+    }
+
+    try {
+      const response = await fetch(
+        'https://jsonplaceholder.typicode.com/users'
+      );
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const users = await response.json();
+      const user = users.find((u) => u.name.toLowerCase() === userName);
+      userCity.textContent = user ? `${user.address.city}` : 'User not found';
+    } catch (error) {
+      console.error('Error fetching users:', error);
+      userCity.textContent = 'Failed to fetch users';
+    }
+  });
+});
